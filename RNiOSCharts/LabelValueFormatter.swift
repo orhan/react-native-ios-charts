3f3d0f243bfaa96869b6cfd47878fd3020e5f3a0
Orhan Sönmez <orhnsnmz@gmail.com>
2016-12-30 15:31:42 +0100

    Removed unnecessary logs

--- RNiOSCharts/LabelValueFormatter.swift
+++ RNiOSCharts/LabelValueFormatter.swift
@@ -19,11 +19,11 @@ open class LabelValueFormatter: NSObject, IAxisValueFormatter
   public typealias Block = (
     _ value: Double,
     _ axis: AxisBase?) -> String
-  
+
   open var block: Block?
-  
+
   open var hasAutoDecimals: Bool = false
-  
+
   fileprivate var _formatter: NumberFormatter?
   open var formatter: NumberFormatter?
     {
@@ -34,7 +34,7 @@ open class LabelValueFormatter: NSObject, IAxisValueFormatter
       _formatter = newValue
     }
   }
-  
+
   fileprivate var _decimals: Int?
   open var decimals: Int?
     {
@@ -42,7 +42,7 @@ open class LabelValueFormatter: NSObject, IAxisValueFormatter
     set
     {
       _decimals = newValue
-      
+
       if let digits = newValue
       {
         self.formatter?.minimumFractionDigits = digits
@@ -51,7 +51,7 @@ open class LabelValueFormatter: NSObject, IAxisValueFormatter
       }
     }
   }
-  
+
   fileprivate var _labelsForValues: [String] = []
   open var labelsForValues: [String]
     {
@@ -61,52 +61,52 @@ open class LabelValueFormatter: NSObject, IAxisValueFormatter
       _labelsForValues = newValue
     }
   }
-  
+
   public override init()
   {
     super.init()
-    
+
     self.formatter = NumberFormatter()
     hasAutoDecimals = true
   }
-  
+
   public init(formatter: NumberFormatter)
   {
     super.init()
-    
+
     self.formatter = formatter
   }
-  
+
   public init(formatter: NumberFormatter, labelsForValues: [String])
   {
     super.init()
-    
+
     self.formatter = formatter
     self.labelsForValues = labelsForValues
   }
-  
+
   public init(decimals: Int)
   {
     super.init()
-    
+
     self.formatter = NumberFormatter()
     self.formatter?.usesGroupingSeparator = true
     self.decimals = decimals
     hasAutoDecimals = true
   }
-  
+
   public init(block: @escaping Block)
   {
     super.init()
-    
+
     self.block = block
   }
-  
+
   public static func with(block: @escaping Block) -> LabelValueFormatter?
   {
     return LabelValueFormatter(block: block)
   }
-  
+
   open func stringForValue(_ value: Double,
                            axis: AxisBase?) -> String
   {
@@ -116,16 +116,10 @@ open class LabelValueFormatter: NSObject, IAxisValueFormatter
     }
     else
     {
-      print("Value");
-      print(value);
-      print("Axis");
-      print(axis);
-      
-      
       if (labelsForValues.count > 0 && labelsForValues.indices.contains(Int(value))) {
         return labelsForValues[Int(value)];
       } else {
-        return formatter?.string(from: NSNumber(floatLiteral: value)) ?? "" 
+        return formatter?.string(from: NSNumber(floatLiteral: value)) ?? ""
       }
     }
   }
